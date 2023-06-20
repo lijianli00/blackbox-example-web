f5c877d588f96eb0aaa79551962be688f3f9e1dc
‘ul’ <136069237@qq.com>
2023-06-20 14:27:28 +0800

    平台管理路由

--- src/router/module/admin.js
+++ src/router/module/admin.js
@@ -1,22 +1,33 @@
-import { RouterError404, RouterError405 } from 'blackbox-ui'
+import {
+  RouterError404,
+  RouterError405,
+  RouterErrorDisable,
+  RouterPlatformUnit,
+  RouterPlatformPerson,
+  RouterPlatformAuth
+} from 'blackbox-ui'
+
 import Layout from '../../layout/index.vue'
 
 export const admin = [
   {
     path: '/',
-    meta: { title: '布局' },
+    meta: { title: '布局', notAuth: true },
     component: Layout,
     children: [
       {
-        path: '/',
+        path: '/index',
         meta: { title: '首页' },
         component: () => import('../../views/system/index/index.vue')
-      }
+      },
+      RouterPlatformUnit,
+      RouterPlatformPerson,
+      RouterPlatformAuth
     ]
   },
   {
     path: '/login',
-    meta: { title: '登录' },
+    meta: { title: '登录', notLogin: true },
     component: () => import('../../views/system/login/index.vue')
   },
   {
@@ -24,6 +35,7 @@ export const admin = [
     meta: { title: '第三方授权登录' },
     component: () => import('../../views/system/ztLogin/index.vue')
   },
+  RouterErrorDisable,
   RouterError405,
   RouterError404
 ]
