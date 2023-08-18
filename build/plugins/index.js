6f5a064555877f8291f7497de90ed17c6a7f7410
李建立 <lijianli0427@163.com>
2023-08-18 08:55:40 +0800

    修改unocss

--- build/plugins/index.js
+++ build/plugins/index.js
@@ -1,37 +1,10 @@
 import vue from '@vitejs/plugin-vue2'
-// import Components from 'unplugin-vue-components/vite'
-// import { visualizer } from "rollup-plugin-visualizer";
-// import commonjs from '@rollup/plugin-commonjs'
-
-import { unocss } from './unocss'
-// import { presetUno, presetAttributify, presetIcons } from 'unocss'
+import unocss from 'unocss/vite'
 import { splitVendorChunkPlugin } from 'vite'
-// import { ElementUiResolver } from 'unplugin-vue-components/resolvers'
-// import requireTransform from 'vite-plugin-require-transform'
-
 // html title
 import { configHtmlPlugin } from './html'
-
 export function createVitePlugins(viteEnv, isBuild) {
-  const plugins = [
-    vue(),
-    splitVendorChunkPlugin(),
-    configHtmlPlugin(viteEnv, isBuild),
-    // Components({
-    //   resolvers: [ElementUiResolver()]
-    // }),
-    // Unocss
-    // requireTransform({
-    //   fileRegex: /.js$|.jsx$ | .vue$/
-    // }),
-    unocss()
-    // commonjs()
-    // visualizer({
-    //   open: true,
-    //   gzipSize: true,
-    //   brotliSize: true,
-    // })
-  ]
+  const plugins = [vue(), splitVendorChunkPlugin(), configHtmlPlugin(viteEnv, isBuild), unocss()]
 
   return plugins
 }
